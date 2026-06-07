63d02583dc31bda8d8efa94911978bf0bbd3a755
BillBaquerizo <billbz9@gmail.com>
2026-06-06 22:20:32 -0400

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,7 @@ public class Main {
         System.out.println(args[0]);
         System.out.println(args[1]);
         System.out.println(args[2]);
+        System.out.println(args[3]);
 
         try (ServerSocket serverSocket = new ServerSocket(4221)) {
 
